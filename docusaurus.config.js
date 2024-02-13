1dd212828228829aa07f9830a7577f72a423b06d
maxjackson2 <mx.senrito2@gmail.com>
2024-02-13 22:37:19 +0300

    test3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 const organizationName = "Max-Bored-Prdouctions";
-const projectName = "MBP";
+const projectName = "info";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
